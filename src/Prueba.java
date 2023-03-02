4fa80f99ba53063e151a23099c3c23d9ae5c9030
Jose Armando Enriquez Luna <armandoenriquez0531@gmail.com>
2023-03-02 13:58:45 -0600

    Se agregaron todos los elementos faltantes y se termino el programa

--- src/Prueba.java
+++ src/Prueba.java
@@ -1,5 +1,7 @@
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.text.MaskFormatter;
+import java.text.ParseException;
 
 class VentanaInicio extends JFrame{
 
@@ -11,7 +13,6 @@ class VentanaInicio extends JFrame{
 		getContentPane().setLayout(gbl);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setTitle("Ventana principal");
-		setSize(500, 300);
 		setLocationRelativeTo(null);
 		setVisible(true);
 		
@@ -71,6 +72,49 @@ class VentanaInicio extends JFrame{
 		llenado(GridBagConstraints.HORIZONTAL, 0, 20, 1, 2, check5);
 		llenado(GridBagConstraints.HORIZONTAL, 0, 21, 1, 2, check6);
 		llenado(GridBagConstraints.HORIZONTAL, 0, 22, 1, 3, check7);
+		
+
+		llenado(GridBagConstraints.HORIZONTAL, 4, 0, 1, 4, new JLabel("                 "));
+		JPanel panel1=new JPanel();
+		panel1.setLayout(new FlowLayout());
+		panel1.setBorder(BorderFactory.createTitledBorder("Preview"));
+		panel1.setPreferredSize(new Dimension(225,200));
+		llenado(GridBagConstraints.WEST,5,1,10,18, panel1);
+
+		JLabel label7=new JLabel("Email Adress");
+		panel1.add(label7).setBounds(0, 0, 10, 15);;
+		MaskFormatter formatter=null;
+		try {
+			formatter=new MaskFormatter("@outlook.com");
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		JFormattedTextField ftf = new JFormattedTextField(formatter);
+		panel1.add(ftf);
+		
+		JLabel label8=new JLabel("First Name");
+		panel1.add(label8);
+
+		JTextField caja3=new JTextField(10);
+		panel1.add(caja3);
+
+		JLabel label9=new JLabel("Last Name");
+		panel1.add(label9);
+
+		JTextField caja4=new JTextField(10);
+		panel1.add(caja4);
+
+		JButton boton=new JButton();
+		boton.setBounds(10, 100, 90, 30);
+		ImageIcon icono=new ImageIcon("Subscribe.png");
+		boton.setIcon(new ImageIcon(icono.getImage().getScaledInstance(boton.getWidth(), boton.getHeight(), Image.SCALE_SMOOTH)));
+		panel1.add(boton);
+
+		JTextArea escribir=new JTextArea("Escribe aqui...",10,25);
+		JScrollPane scroll=new JScrollPane(escribir);
+		llenado(GridBagConstraints.HORIZONTAL,4,11,30,30, scroll);
+		pack();
+
 
 	}
 
